70a8d88c35808fdaafd6c8058ed0f413bb6b1b22
Agustin Barrionuevo <barrionuevoagustin289@gmail.com>
2022-11-06 14:07:42 -0300

    Router Videogames

--- api/src/routes/Controllers/Controllers.js
+++ api/src/routes/Controllers/Controllers.js
@@ -2,14 +2,29 @@ require('dotenv').config();
 const {
 	API_KEY
   } = process.env;
-const axios = require('axios')
+const axios = require('axios');
 
+
+//Busca todos los generos de la API ---------------------------------------------------
 async function getAllGenres () {
 	const allGenres = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
 	.then(response => response.data)
 	return allGenres
 }
 
+//Busca los videojuegos de la API y busca los videojuegos de la api que coincidan con el nombre que recibe por parametro --------
+async function getVideogames (name) {
+	if(name){
+		const videogamesByName = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`)
+		.then(response => response.data)
+		return videogamesByName
+	}
+	const videogames = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+	.then(response => response.data)
+	return videogames
+}
+
 module.exports = {
-	getAllGenres
+	getAllGenres,
+	getVideogames,
 }
--- api/src/routes/videogamesRouter.js
+++ api/src/routes/videogamesRouter.js
@@ -2,12 +2,20 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const videogamesRouter = Router();
+const { getVideogames } = require('./Controllers/Controllers')
 
 videogamesRouter.get('/', async (req, res)=> {
+	const { name } = req.query
 	try {
-		
+		if(name) {
+			let searchByName = await getVideogames(name)
+			if(searchByName.count === 0)return res.status(404).send('No existe ningun juego con ese nombre')
+			return res.status(200).send(searchByName)
+		}
+		let videogames = await getVideogames()
+		return res.status(200).send(videogames)
 	} catch (error) {
-		
+		return res.status(404).send(error.message)
 	}
 })
 
